989f0f99c7262d380ae27246f2b1602ae9acc495
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2022-04-05 11:36:19 +0200

    Přidejte soubory projektu.

---
+++ KostkaHra/Kostka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kostky
+{
+    class Kostka
+    {
+        private int hodnota;
+
+        public int Hodnota { get => hodnota; }
+
+        public void Hod()
+        {
+            Random random = new Random();
+            hodnota = random.Next(1, 7);
+
+        }
+
+        public Kostka()
+        {
+            Hod();
+        }
+    }
+}
